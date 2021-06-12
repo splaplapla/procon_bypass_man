0a6ae98e016522d5c15093acca96f2bf3803c1c3
jiikko <n905i.1214@gmail.com>
2021-06-12 16:51:38 +0900

    a

--- lib/procon_bypass_man.rb
+++ lib/procon_bypass_man.rb
@@ -1,6 +1,7 @@
 require_relative "procon_bypass_man/version"
 require_relative "procon_bypass_man/device_registry"
 require_relative "procon_bypass_man/runner"
+require_relative "procon_bypass_man/processor"
 require_relative "procon_bypass_man/plugin_integration"
 
 STDOUT.sync = true
--- lib/procon_bypass_man/processor.rb
+++ lib/procon_bypass_man/processor.rb
@@ -1,13 +1,11 @@
-module ProconBypassMan
-  module Processor
-    # @return [String] binary
-    def initialize(binary)
-      @binary = binary
-    end
+module ProconBypassMan::Processor
+  # @return [String] binary
+  def initialize(binary)
+    @binary = binary
+  end
 
-    # @return [String]
-    def process
-      @binary
-    end
+  # @return [String]
+  def process
+    @binary
   end
 end
