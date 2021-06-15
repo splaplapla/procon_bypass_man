74a66bbd947a9dac138ed4cbb609bb6fc65b8f47
jiikko <n905i.1214@gmail.com>
2021-06-15 20:45:53 +0900

    support channel

--- lib/procon_bypass_man/procon.rb
+++ lib/procon_bypass_man/procon.rb
@@ -175,6 +175,7 @@ class ProconBypassMan::Procon
       self.binary[11] = auto_binary[11]
       return
     else
+      consumed_channel_table = {}
       flip_buttons.each do |button, options|
         unless options[:if_pushed]
           status[button] = !status[button]
@@ -182,7 +183,10 @@ class ProconBypassMan::Procon
         end
 
         if options[:if_pushed] && options[:if_pushed].all? { |b| pushed_button?(b) }
+          # 同じチャンネルで操作済みのときはステータスを更新しない
+          next if consumed_channel_table[options[:channel]]
           status[button] = !status[button]
+          consumed_channel_table[options[:channel]] = true if options[:channel]
         else
           status[button] = false
         end
