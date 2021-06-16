21c6c3516d3ed085b6100e9b7b2511fbfc0963f4
jiikko <n905i.1214@gmail.com>
2021-06-16 20:52:34 +0900

    add spec

--- spec/lib/procon_bypass_man/procon/mode_registry_spec.rb
+++ spec/lib/procon_bypass_man/procon/mode_registry_spec.rb
@@ -2,6 +2,26 @@ require "spec_helper"
 
 describe ProconBypassMan::Procon::ModeRegistry do
   before(:each) { ProconBypassMan::Procon::ModeRegistry.reset! }
+  describe '.load' do
+    it do
+      module Hoge2Mode
+        def self.mode_name
+          :hoge
+        end
+
+        def self.binaries
+          [:a, :b, :y]
+        end
+      end
+
+      ::ProconBypassMan::Procon::ModeRegistry.install_plugin(Hoge2Mode)
+      mode = ProconBypassMan::Procon::ModeRegistry.load(:hoge)
+      expect(mode.next_binary).to eq(:a)
+      expect(mode.next_binary).to eq(:b)
+      expect(mode.next_binary).to eq(:y)
+      expect(mode.next_binary).to eq(:a)
+    end
+  end
   describe '.install_plugin' do
     it do
       module HogeMode
