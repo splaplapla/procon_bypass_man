2fe20878e75dc975bd97725867f8f4e31e124b98
jiikko <n905i.1214@gmail.com>
2021-06-19 09:47:25 +0900

    けしてみる

--- lib/procon_bypass_man/bypass.rb
+++ lib/procon_bypass_man/bypass.rb
@@ -7,50 +7,30 @@ class ProconBypassMan::Bypass
   end
 
   def send_gadget_to_procon!
-    monitor.record(:start_function)
     begin
-      monitor.record(:before_read)
       input = self.gadget.read_nonblock(128)
-      monitor.record(:after_read)
-      #rescue IO::EAGAINWaitReadable
-      #  monitor.record(:eagain_wait_readable_on_read)
-      #  retry
-      #end
-
-      monitor.record(:before_write)
       self.procon.write_nonblock(input)
-      monitor.record(:after_write)
       sleep($will_interval_1_6)
     rescue IO::EAGAINWaitReadable
-      monitor.record(:eagain_wait_readable_on_write)
       sleep($will_interval_1_6)
       return
     end
-    monitor.record(:end_function)
   end
 
   def send_procon_to_gadget!
-    monitor.record(:start_function)
     output = nil
     begin
-      monitor.record(:before_read)
       output = self.procon.read_nonblock(128)
-      monitor.record(:after_read)
     rescue IO::EAGAINWaitReadable
-      monitor.record(:eagain_wait_readable_on_read)
       retry
     end
 
     begin
       ProconBypassMan.logger.debug { "<<< #{output.unpack("H*")}" }
-      monitor.record(:before_write)
       self.gadget.write_nonblock(ProconBypassMan::Processor.new(output).process)
-      monitor.record(:after_write)
       sleep($will_interval_0_0_1)
     rescue IO::EAGAINWaitReadable
-      monitor.record(:eagain_wait_readable_on_write)
       return
     end
-    monitor.record(:end_function)
   end
 end
