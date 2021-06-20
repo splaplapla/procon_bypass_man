a3210f16de0758c58d01c9313e02d1ab41c922e5
jiikko <n905i.1214@gmail.com>
2021-06-20 22:14:01 +0900

    USR2シグナルを受け取ったらmain loopのプロセスを再起動する

--- lib/procon_bypass_man/runner.rb
+++ lib/procon_bypass_man/runner.rb
@@ -1,6 +1,8 @@
 require_relative "io_monitor"
 
 class ProconBypassMan::Runner
+  class InterruptForRestart < StandardError; end
+
   def initialize(gadget: , procon: )
     @gadget = gadget
     @procon = procon
@@ -11,7 +13,45 @@ class ProconBypassMan::Runner
 
   def run
     first_negotiation
-    main_loop
+    $is_stable = false
+
+    self_read, self_write = IO.pipe
+    %w(TERM INT USR2).each do |sig|
+      begin
+        trap sig do
+          self_write.puts(sig)
+        end
+      rescue ArgumentError
+        ProconBypassMan.logger.info("Signal #{sig} not supported")
+      end
+    end
+
+    loop do
+      main_loop_pid = fork { main_loop }
+
+      begin
+        while readable_io = IO.select([self_read])
+          signal = readable_io.first[0].gets.strip
+          handle_signal(signal)
+        end
+      rescue InterruptForRestart
+        $will_terminate_token = true
+        [t1, t2].each(&:join)
+        Process.kill("TERM", main_loop_pid)
+        Process.wait
+        ProconBypassMan.logger.info("Reloading config file")
+        # reload config
+        ProconBypassMan.logger.info("バイパス処理を再開します")
+      rescue Interrupt
+        $will_terminate_token = true
+        [t1, t2].each(&:join)
+        Process.kill("TERM", main_loop_pid)
+        Process.wait
+        @gadget&.close
+        @procon&.close
+        exit 1
+      end
+    end
   end
 
   private
@@ -24,7 +64,6 @@ class ProconBypassMan::Runner
       $will_interval_1_6 = 1.6
       $is_stable = true
     end
-    $is_stable = false
 
     ProconBypassMan::IOMonitor.start!
     # gadget => procon
@@ -106,6 +145,8 @@ class ProconBypassMan::Runner
   def handle_signal(sig)
     ProconBypassMan.logger.info "#{sig}を受け取りました"
     case sig
+    when 'USR2'
+      raise InterruptForRestart
     when 'INT', 'TERM'
       raise Interrupt
     end
