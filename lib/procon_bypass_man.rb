3473cca8a0cad976077d2137459b6ca43c7b59f9
jiikko <n905i.1214@gmail.com>
2021-07-18 22:23:22 +0900

    bundlerのデータ不整合が起きた時の例外処理を追加する

--- lib/procon_bypass_man.rb
+++ lib/procon_bypass_man.rb
@@ -80,4 +80,27 @@ module ProconBypassMan
   def self.root=(path)
     @@root = path
   end
+
+  # bundle install相当の処理中にプロセスが死ぬとファイルの整合性が取れなくて死に続ける問題への対策
+  # 原文ママ: <Bundler::Source::Git::GitCommandError: Git error: command `git fetch --force --quiet --tags https://github.com/splaspla-hacker/procon_bypass_man-web refs/heads/\*:refs/heads/\*` in directory /home/pi/.rbenv/versions/3.0.1/lib/ruby/gems/3.0.0/cache/bundler/git/procon_bypass_man-web-684bff71720a35ca5928a1af9f6458aff01a8fd1 has failed. If this error persists you could try removing the cache directory '/home/pi/.rbenv/versions/3.0.1/lib/ruby/gems/3.0.0/cache/bundler/git/procon_bypass_man-web-684bff71720a35ca5928a1af9f6458aff01a8fd1'>
+  def self.remove_bundler_cache_if_need(&block)
+    begin
+      block.call
+    # TODO  bundlerのバージョンを指定する
+    rescue Bundler::Source::Git::GitCommandError => e
+      if %r!If this error persists you could try removing the cache directory '([^']+)'! =~ e.to_s
+        if is_correct_directory_to_remove?($1)
+          FileUtiles.rm_rf($1)
+          puts "Bundler::Source::Git::GitCommandErrorが起きたので問題のディレクトリを削除しました。"
+        else
+          raise "bundlerのキャッシュディレクトリを削除できませんでした"
+        end
+      end
+      retry
+    end
+  end
+
+  def self.is_correct_directory_to_remove?(dir)
+    !!(%r!^/home/pi/.rbenv/versions/! =~ dir)
+  end
 end
