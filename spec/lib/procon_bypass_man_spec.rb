63bfb66de70727f4a3da21ad4071a8bbe434ba21
jiikko <n905i.1214@gmail.com>
2021-07-18 22:58:00 +0900

    これいみなかった

--- spec/lib/procon_bypass_man_spec.rb
+++ spec/lib/procon_bypass_man_spec.rb
@@ -6,19 +6,4 @@ describe ProconBypassMan do
       expect(described_class.logger).not_to be_nil
     end
   end
-
-  describe '.is_correct_directory_to_remove?' do
-    context '/' do
-      it { expect(ProconBypassMan.is_correct_directory_to_remove?("/")).to eq(false) }
-    end
-    context '/etc' do
-      it { expect(ProconBypassMan.is_correct_directory_to_remove?("/etc")).to eq(false) }
-    end
-    context '/home/hoge' do
-      it { expect(ProconBypassMan.is_correct_directory_to_remove?('/home/hoge')).to eq(false) }
-    end
-    context '/home/pi/.rbenv/versions/3.0.1' do
-      it { expect(ProconBypassMan.is_correct_directory_to_remove?('/home/pi/.rbenv/versions/3.0.1')).to eq(true) }
-    end
-  end
 end
