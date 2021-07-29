238b56bd2b3280d4f9c81c5bbaae2a70711a21b5
jiikko <n905i.1214@gmail.com>
2021-07-29 21:39:57 +0900

    validationに名前をつける

--- lib/procon_bypass_man/configuration/validator.rb
+++ lib/procon_bypass_man/configuration/validator.rb
@@ -4,11 +4,27 @@ module ProconBypassMan
       # @return [Boolean]
       def valid?
         @errors = Hash.new {|h,k| h[k] = [] }
-        if prefix_keys.empty?
-          @errors[:prefix_keys] ||= []
-          @errors[:prefix_keys] << "prefix_keys_for_changing_layerに値が入っていません。"
-        end
 
+        validate_require_prefix_keys
+        validate_config_of_button_lonely
+        validate_verify_button_existence
+
+        @errors.empty?
+      end
+
+      # @return [Boolean]
+      def invalid?
+        !valid?
+      end
+
+      # @return [Hash]
+      def errors
+        @errors ||= Hash.new {|h,k| h[k] = [] }
+      end
+
+      private
+
+      def validate_config_of_button_lonely
         @layers.each do |layer_key, value|
           if ProconBypassMan::Procon::ModeRegistry::PRESETS.key?(value.mode)
             next
@@ -18,7 +34,16 @@ module ProconBypassMan
             end
           end
         end
+      end
 
+      def validate_require_prefix_keys
+        if prefix_keys.empty?
+          @errors[:prefix_keys] ||= []
+          @errors[:prefix_keys] << "prefix_keys_for_changing_layerに値が入っていません。"
+        end
+      end
+
+      def validate_verify_button_existence
         @layers.each do |layer_key, value|
           unverified_buttons = []
           # teplevel target button
@@ -46,18 +71,6 @@ module ProconBypassMan
             @errors[:layers] << "#{layer_key}で存在しないボタン#{nunsupport_buttons.join(", ")}があります"
           end
         end
-
-        @errors.empty?
-      end
-
-      # @return [Boolean]
-      def invalid?
-        !valid?
-      end
-
-      # @return [Hash]
-      def errors
-        @errors ||= Hash.new {|h,k| h[k] = [] }
       end
     end
   end
