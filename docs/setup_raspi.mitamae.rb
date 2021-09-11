62f89d025898c473cc87cbebd3c3c6ddb350bda6
jiikko <n905i.1214@gmail.com>
2021-09-11 10:50:09 +0900

    mkdir

--- docs/setup_raspi.mitamae.rb
+++ docs/setup_raspi.mitamae.rb
@@ -31,7 +31,7 @@ end
 # PBM
 execute "Initialize PBM" do
   command <<~SHELL
-    mkdir -p /usr/share/pbm/shared
+    sudo mkdir -p /usr/share/pbm/shared
     wget https://gist.githubusercontent.com/jiikko/3f9fb3194c0cc7685e31fbfcb5b5f9ff/raw/23ddee29d94350be80b79d290ac3c8ce8400bd88/add_procon_gadget.sh
     mv add_procon_gadget.sh /usr/share/pbm/shared/add_procon_gadget.sh
     chmod +x /usr/share/pbm/shared/add_procon_gadget.sh
