043b1b0c0db57715e2bfd5330e9d879efe1d1a3a
jiikko <n905i.1214@gmail.com>
2021-09-28 22:02:00 +0900

    readしたら文字を返す

--- spec/lib/procon_bypass_man/bypass_spec.rb
+++ spec/lib/procon_bypass_man/bypass_spec.rb
@@ -12,7 +12,9 @@ describe ProconBypassMan::Bypass do
   end
 
   describe '.send_procon_to_gadget!' do
+    let(:output) { ["3075810080009ea7734758750c56fbdc02240f220007001c0055fbdc02250f250009001d005cfbde02240f230007001c00000000000000000000000000000000"].pack("H*") }
     it do
+      expect(dev).to receive(:read) { output }
       monitor = ProconBypassMan::IOMonitor.new(label: "gadget => procon")
       bypass = ProconBypassMan::Bypass.new(gadget: dev, procon: dev, monitor: monitor)
       expect(bypass).to receive(:push_gadget_to_switch_queue)
