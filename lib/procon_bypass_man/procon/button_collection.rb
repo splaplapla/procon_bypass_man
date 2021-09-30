08934ab7081bb849b4651aa54a9e46b00f594964
jiikko <n905i.1214@gmail.com>
2021-09-30 22:51:39 +0900

    add comment

--- lib/procon_bypass_man/procon/button_collection.rb
+++ lib/procon_bypass_man/procon/button_collection.rb
@@ -8,15 +8,20 @@ class ProconBypassMan::Procon::ButtonCollection
     end
   end
 
+  # https://github.com/dekuNukem/Nintendo_Switch_Reverse_Engineering/blob/ac8093c84194b3232acb675ac1accce9bcb456a3/bluetooth_hid_notes.md
+  #0) Input report ID
+  #1) Timer. Increments very fast. Can be used to estimate excess Bluetooth latency.
+  #2 high nibble) Battery level. 8=full, 6=medium, 4=low, 2=critical, 0=empty. LSB=Charging.
+  #2 low nibble) Connection info. (con_info >> 1) & 3 - 3=JC, 0=Pro/ChrGrip. con_info & 1 - 1=Switch/USB powered.
   #3)  ZR	R	SR(right)	SL(right)	A	B	X	Y
   #4)  Grip	(none)	Cap	Home	ThumbL	ThumbR	+	-
   #5)  ZL	L	SL(left)	SR(left)	Left	Right	Up	Down
-  #6)  analog[0]
-  #7)  analog[1]
-  #8)  analog[2]
-  #9)  analog[3]
-  #a)  analog[4]
-  #b)  analog[5]
+  #6)  analog[0] Left analog stick data
+  #7)  analog[1] Left analog stick data
+  #8)  analog[2] Left analog stick data
+  #9)  analog[3] Right analog stick data
+  #a)  analog[4] Right analog stick data
+  #b)  analog[5] Right analog stick data
   BYTES_MAP = {
     0 => nil,
     1 => nil,
