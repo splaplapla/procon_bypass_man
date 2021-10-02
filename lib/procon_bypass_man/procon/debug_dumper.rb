2987ad2c40c22bd8ddd15772d161c8c9c5fdd54b
jiikko <n905i.1214@gmail.com>
2021-10-02 21:23:55 +0900

    dump

---
+++ lib/procon_bypass_man/procon/debug_dumper.rb
@@ -0,0 +1,12 @@
+class ProconBypassMan::Procon::DebugDumper
+  def initialize(binary: )
+    @binary = binary
+  end
+
+  def dump_analog_sticks
+    fi = @binary[6..8].unpack("H*").first.to_i(16).to_s(2).rjust(18, "0")
+    se = @binary[9..11].unpack("H*").first.to_i(16).to_s(2).rjust(18, "0")
+    th = @binary[12..14].unpack("H*").first.to_i(16).to_s(2).rjust(18, "0")
+    ProconBypassMan.logger.debug "6..8: #{fi}, 9..11: #{se}, 12..14: #{th}"
+  end
+end
