5005c78514dbdf48a7ef5330207e83bff16ecd8a
jiikko <n905i.1214@gmail.com>
2021-10-06 02:28:05 +0900

    fix bug

--- lib/procon_bypass_man/procon/analog_stick_cap.rb
+++ lib/procon_bypass_man/procon/analog_stick_cap.rb
@@ -13,7 +13,7 @@ class ProconBypassMan::Procon::AnalogStickCap
         ((@y << 4) & "0xf0".to_i(16)) | ((@x >> 8) & "0x0f".to_i(16)),
         (@y >> 4) & "0xff".to_i(16),
       ]
-      hex = analog_stick_data.map{ |x| x.to_s(16).ljust(2, "0") }.join
+      hex = analog_stick_data.map{ |x| x.to_s(16).rjust(2, "0") }.join
       [hex].pack("H*")
     end
   end
