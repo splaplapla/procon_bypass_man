2076ab482af652d5c11a7fa8213fbbf8f0baf007
jiikko <n905i.1214@gmail.com>
2021-10-31 02:27:29 +0900

    unpackerだけをクラスに切り出す

--- spec/lib/procon_bypass_man/readonly_pro_controller_spec.rb
+++ spec/lib/procon_bypass_man/readonly_pro_controller_spec.rb
@@ -5,8 +5,10 @@ describe ProconBypassMan::ReadonlyProController do
   let(:pressed_y_and_b) { "30778105800099277344e86b0a7909f4f5a8f4b500c5ff8dff6c09cdf5b8f49a00c5ff92ff6a0979f5eef46500d5ff9bff000000000000000000000000000000" }
   let(:data) { pressed_y_and_b }
 
-  it do
-    actual = ProconBypassMan::ReadonlyProController.new(binary: binary).pressed
-    expect(actual).to include(:y=>true, :b=>true)
+  describe '#pressed' do
+    it do
+      actual = ProconBypassMan::ReadonlyProController.new(binary: binary).pressed
+      expect(actual).to include(:y=>true, :b=>true)
+    end
   end
 end
