9944f0f6ff31e6e8df541d96cbdab2a82479ba0c
jiikko <n905i.1214@gmail.com>
2021-11-10 13:19:41 +0900

    rename

--- lib/procon_bypass_man/bypass.rb
+++ lib/procon_bypass_man/bypass.rb
@@ -3,13 +3,13 @@ require "procon_bypass_man/bypass/usb_hid_logger"
 class ProconBypassMan::Bypass
   include ProconBypassMan::Bypass::UsbHidLogger
 
-  class BypassStatus < Struct.new(:binary, :sent)
+  class BypassValue < Struct.new(:binary, :sent)
     def to_text
       "#{binary.unpack("H*").first} #{'x' unless sent}"
     end
   end
 
-  attr_accessor :gadget, :procon, :monitor, :bypass_status
+  attr_accessor :gadget, :procon, :monitor, :bypass_value
 
   def initialize(gadget: , procon: , monitor: )
     self.gadget = gadget
@@ -21,14 +21,14 @@ class ProconBypassMan::Bypass
   def send_gadget_to_procon!
     monitor.record(:start_function)
     input = nil
-    self.bypass_status = BypassStatus.new(input, sent = false)
+    self.bypass_value = BypassValue.new(input, sent = false)
 
     run_callbacks(:send_gadget_to_procon) do
       begin
         break if $will_terminate_token
         # TODO blocking readにしたいが、接続時のフェーズによって長さが違うので厳しい
         input = self.gadget.read_nonblock(64)
-        self.bypass_status.binary = input
+        self.bypass_value.binary = input
       rescue IO::EAGAINWaitReadable
         monitor.record(:eagain_wait_readable_on_read)
         sleep(0.001)
@@ -37,7 +37,7 @@ class ProconBypassMan::Bypass
 
       begin
         self.procon.write_nonblock(input)
-        self.bypass_status.sent = true
+        self.bypass_value.sent = true
       rescue IO::EAGAINWaitReadable
         monitor.record(:eagain_wait_readable_on_write)
         break
@@ -50,14 +50,14 @@ class ProconBypassMan::Bypass
   def send_procon_to_gadget!
     monitor.record(:start_function)
     output = nil
-    self.bypass_status = BypassStatus.new(output, sent = false)
+    self.bypass_value = BypassValue.new(output, sent = false)
 
     run_callbacks(:send_procon_to_gadget) do
       begin
         break if $will_terminate_token
         Timeout.timeout(1) do
           output = self.procon.read(64)
-          self.bypass_status.binary = output
+          self.bypass_value.binary = output
         end
       rescue Timeout::Error
         ProconBypassMan.logger.debug { "read timeout! do sleep. by send_procon_to_gadget!" }
@@ -73,7 +73,7 @@ class ProconBypassMan::Bypass
 
       begin
         self.gadget.write_nonblock(ProconBypassMan::Processor.new(output).process)
-        self.bypass_status.sent = true
+        self.bypass_value.sent = true
       rescue IO::EAGAINWaitReadable
         monitor.record(:eagain_wait_readable_on_write)
         break
--- lib/procon_bypass_man/bypass/usb_hid_logger.rb
+++ lib/procon_bypass_man/bypass/usb_hid_logger.rb
@@ -10,21 +10,21 @@ class ProconBypassMan::Bypass
     set_callback :send_procon_to_gadget, :after, :log_procon_to_gadget
 
     def log_send_gadget_to_procon
-      ProconBypassMan.logger.debug { ">>> #{bypass_status.to_text}" }
+      ProconBypassMan.logger.debug { ">>> #{bypass_value.to_text}" }
     end
 
     def log_procon_to_gadget
       if ProconBypassMan.config.verbose_bypass_log
-        ProconBypassMan.logger.debug { "<<< #{bypass_status.to_text}" }
+        ProconBypassMan.logger.debug { "<<< #{bypass_value.to_text}" }
       else
         ProconBypassMan.cache.fetch key: 'bypass_log', expires_in: 1 do
-          ProconBypassMan.logger.debug { "<<< #{bypass_status.to_text}" }
+          ProconBypassMan.logger.debug { "<<< #{bypass_value.to_text}" }
         end
       end
 
       ProconBypassMan.cache.fetch key: 'reporter', expires_in: 5 do
         ProconBypassMan::Outbound::Worker.push({
-          data: ProconBypassMan::ReadonlyProcon.new(binary: bypass_status.binary).to_hash,
+          data: ProconBypassMan::ReadonlyProcon.new(binary: bypass_value.binary).to_hash,
           reporter_class: ProconBypassMan::PressedButtonsReporter
         })
       end
