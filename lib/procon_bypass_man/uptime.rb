70b9cf56bf7c22a1b4c739d9d7941aebd8b81309
jiikko <n905i.1214@gmail.com>
2021-11-11 20:12:34 +0900

    起動時間の表示を正しくする

--- lib/procon_bypass_man/uptime.rb
+++ lib/procon_bypass_man/uptime.rb
@@ -2,10 +2,20 @@ require "time"
 
 module ProconBypassMan
   class Uptime
+    # @return [Integer]
     def self.from_boot
-      result = `uptime -s`.chomp
-      return -1 if result == '' # darwin系だとsオプションが使えない
-      boot_time = result.to_i
+      new(uptime_cmd_result: `uptime -s`.chomp).from_boot
+    end
+
+    # @param [String] uptime_cmd_result
+    def initialize(uptime_cmd_result: )
+      @result = uptime_cmd_result
+    end
+
+    # @return [Integer]
+    def from_boot
+      return -1 if @result == '' # darwin系だとsオプションが使えない
+      boot_time = Time.parse(@result).to_i
       return Time.now.to_i - boot_time.to_i
     rescue => e
       ProconBypassMan.logger.error(e)
