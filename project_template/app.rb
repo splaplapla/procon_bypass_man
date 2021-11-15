45281a3c6be93a7d679d056770c381341d10b8cb
jiikko <n905i.1214@gmail.com>
2021-11-15 21:32:00 +0900

    fix doc

--- project_template/app.rb
+++ project_template/app.rb
@@ -5,7 +5,7 @@ require 'bundler/inline'
 gemfile do
   source 'https://rubygems.org'
   git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
-  gem 'procon_bypass_man', '0.1.11'
+  gem 'procon_bypass_man', '0.1.12'
   gem 'procon_bypass_man-splatoon2', github: 'splaplapla/procon_bypass_man-splatoon2', tag: "0.1.1"
 end
 
