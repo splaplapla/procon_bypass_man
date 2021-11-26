ab52cada71dd00f96dd4ed7287a93a6f51133c8f
jiikko <n905i.1214@gmail.com>
2021-11-27 02:02:42 +0900

    add spec

---
+++ spec/lib/procon_bypass_man/commands/run_remote_pbm_job_command_spec.rb
@@ -0,0 +1,44 @@
+require "spec_helper"
+
+describe ProconBypassMan::RunRemotePbmActionCommand do
+  describe '.execute' do
+    subject { ProconBypassMan::RunRemotePbmActionCommand.execute(action: action, uuid: "a") }
+
+    context ProconBypassMan::RemotePbmAction::CHANGE_PBM_VERSION do
+      let(:action) { ProconBypassMan::RemotePbmAction::CHANGE_PBM_VERSION }
+
+      it do
+        expect(ProconBypassMan::RemotePbmAction::ChangePbmVersionAction).to receive(:new) { double(:o).as_null_object }
+        subject
+      end
+    end
+
+    context ProconBypassMan::RemotePbmAction::REBOOT_PBM do
+      let(:action) { ProconBypassMan::RemotePbmAction::REBOOT_PBM }
+
+      it do
+        expect(ProconBypassMan::RemotePbmAction::RebootPbmAction).to receive(:new) { double(:o).as_null_object }
+        subject
+      end
+    end
+
+    context ProconBypassMan::RemotePbmAction::REBOOT_OS do
+      let(:action) { ProconBypassMan::RemotePbmAction::REBOOT_OS }
+
+      it do
+        expect(ProconBypassMan::RemotePbmAction::RebootOsAction).to receive(:new) { double(:o).as_null_object }
+        subject
+      end
+    end
+
+    context 'when to raise ActionUnexpectedError' do
+      let(:action) { ProconBypassMan::RemotePbmAction::REBOOT_OS }
+
+      it do
+        expect(ProconBypassMan::RemotePbmAction::RebootOsAction).to receive(:new) { raise ProconBypassMan::RemotePbmAction::ActionUnexpectedError }
+        expect(ProconBypassMan::SendErrorCommand).to receive(:execute)
+        subject
+      end
+    end
+  end
+end
