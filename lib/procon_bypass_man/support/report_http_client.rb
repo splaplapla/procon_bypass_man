3d7cd8045fca812fab355d7a1f1528f53a18a37f
jiikko <n905i.1214@gmail.com>
2021-11-28 01:29:10 +0900

    サーバに遅れていなかったのを修正する

--- lib/procon_bypass_man/support/report_http_client.rb
+++ lib/procon_bypass_man/support/report_http_client.rb
@@ -1,12 +1,18 @@
 module ProconBypassMan
   class ReportHttpClient < HttpClient
     def post(body: , event_type: )
+      if body.is_a?(String)
+        b = { text: body }
+      else
+        b = body
+      end
+
       super(request_body: {
         session_id: ProconBypassMan.session_id,
         device_id: ProconBypassMan.device_id,
         hostname: `hostname`.chomp,
         event_type: event_type,
-        body: body.to_json,
+        body: b,
       })
     end
   end
