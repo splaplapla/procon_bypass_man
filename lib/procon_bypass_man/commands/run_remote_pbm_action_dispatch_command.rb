8f9ddff7f72c2922d19dd4d7e8ad93fe0786a479
jiikko <n905i.1214@gmail.com>
2021-11-27 15:34:22 +0900

    rename consts

--- lib/procon_bypass_man/commands/run_remote_pbm_action_dispatch_command.rb
+++ lib/procon_bypass_man/commands/run_remote_pbm_action_dispatch_command.rb
@@ -1,11 +1,11 @@
 class ProconBypassMan::RunRemotePbmActionDispatchCommand
   def self.execute(action: , uuid: )
     case action
-    when ProconBypassMan::RemotePbmAction::CHANGE_PBM_VERSION
+    when ProconBypassMan::RemotePbmAction::ACTION_CHANGE_PBM_VERSION
       ProconBypassMan::RemotePbmAction::ChangePbmVersionAction.new(pbm_job_uuid: uuid).run!
-    when ProconBypassMan::RemotePbmAction::REBOOT_PBM
+    when ProconBypassMan::RemotePbmAction::ACTION_REBOOT_PBM
       ProconBypassMan::RemotePbmAction::RebootPbmAction.new(pbm_job_uuid: uuid).run!
-    when ProconBypassMan::RemotePbmAction::REBOOT_OS
+    when ProconBypassMan::RemotePbmAction::ACTION_REBOOT_OS
       ProconBypassMan::RemotePbmAction::RebootOsAction.new(pbm_job_uuid: uuid).run!
     else
       ProconBypassMan::SendErrorCommand.execute(error: "#{action}は対応していないアクションです")
--- spec/lib/procon_bypass_man/commands/run_remote_pbm_action_dispatch_command_spec.rb
+++ spec/lib/procon_bypass_man/commands/run_remote_pbm_action_dispatch_command_spec.rb
@@ -4,8 +4,8 @@ describe ProconBypassMan::RunRemotePbmActionDispatchCommand do
   describe '.execute' do
     subject { described_class.execute(action: action, uuid: "a") }
 
-    context ProconBypassMan::RemotePbmAction::CHANGE_PBM_VERSION do
-      let(:action) { ProconBypassMan::RemotePbmAction::CHANGE_PBM_VERSION }
+    context ProconBypassMan::RemotePbmAction::ACTION_CHANGE_PBM_VERSION do
+      let(:action) { ProconBypassMan::RemotePbmAction::ACTION_CHANGE_PBM_VERSION }
 
       it do
         expect(ProconBypassMan::RemotePbmAction::ChangePbmVersionAction).to receive(:new) { double(:o).as_null_object }
@@ -13,8 +13,8 @@ describe ProconBypassMan::RunRemotePbmActionDispatchCommand do
       end
     end
 
-    context ProconBypassMan::RemotePbmAction::REBOOT_PBM do
-      let(:action) { ProconBypassMan::RemotePbmAction::REBOOT_PBM }
+    context ProconBypassMan::RemotePbmAction::ACTION_REBOOT_PBM do
+      let(:action) { ProconBypassMan::RemotePbmAction::ACTION_REBOOT_PBM }
 
       it do
         expect(ProconBypassMan::RemotePbmAction::RebootPbmAction).to receive(:new) { double(:o).as_null_object }
@@ -22,8 +22,8 @@ describe ProconBypassMan::RunRemotePbmActionDispatchCommand do
       end
     end
 
-    context ProconBypassMan::RemotePbmAction::REBOOT_OS do
-      let(:action) { ProconBypassMan::RemotePbmAction::REBOOT_OS }
+    context ProconBypassMan::RemotePbmAction::ACTION_REBOOT_OS do
+      let(:action) { ProconBypassMan::RemotePbmAction::ACTION_REBOOT_OS }
 
       it do
         expect(ProconBypassMan::RemotePbmAction::RebootOsAction).to receive(:new) { double(:o).as_null_object }
@@ -32,7 +32,7 @@ describe ProconBypassMan::RunRemotePbmActionDispatchCommand do
     end
 
     context 'when to raise ActionUnexpectedError' do
-      let(:action) { ProconBypassMan::RemotePbmAction::REBOOT_OS }
+      let(:action) { ProconBypassMan::RemotePbmAction::ACTION_REBOOT_OS }
 
       it do
         expect(ProconBypassMan::RemotePbmAction::RebootOsAction).to receive(:new) { raise ProconBypassMan::RemotePbmAction::ActionUnexpectedError }
