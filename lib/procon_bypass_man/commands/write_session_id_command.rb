924b984d0f82ea9ade0bbf72e05a297048c2e155
jiikko <n905i.1214@gmail.com>
2021-11-29 23:21:23 +0900

    session_idはオンメモリにする

--- lib/procon_bypass_man/commands/write_session_id_command.rb
+++ lib/procon_bypass_man/commands/write_session_id_command.rb
@@ -2,12 +2,6 @@ class ProconBypassMan::WriteSessionIdCommand
   # @return [String] session_id ラズパイが起動してからshutdownするまで同じ文字列を返す
   # 起動すると/tmp がなくなる前提の実装
   def self.execute
-    path = "/tmp/pbm_session_id"
-    if(sid = File.read(path))
-      return sid
-    end
-  rescue Errno::ENOENT
-    File.write(path, "s_#{SecureRandom.uuid}")
-    return SecureRandom.uuid
+    @@session_id ||= "s_#{SecureRandom.uuid}"
   end
 end
