5d623d5cfac77ae906a07f34f79ca56c7dd038ce
jiikko <n905i.1214@gmail.com>
2021-12-08 15:45:55 +0900

    add spec

---
+++ spec/lib/procon_bypass_man/remote_pbm_action/restore_pbm_setting_spec.rb
@@ -0,0 +1,40 @@
+require "spec_helper"
+
+describe ProconBypassMan::RemotePbmAction::RestorePbmSettingAction do
+  describe '#run!' do
+    let(:action) { described_class.new(pbm_job_uuid: "a") }
+    subject { action.run!(job_args: {}) }
+
+    context 'エラーが起きないとき' do
+      before do
+        expect(action).to receive(:action_content)
+      end
+
+      it do
+        expect(action).to receive(:be_in_progress)
+        expect(action).to receive(:be_processed)
+        expect(action).not_to receive(:be_failed)
+        subject
+      end
+    end
+
+    context 'エラーが起きるとき' do
+      before do
+        allow(action).to receive(:action_content) { raise RuntimeError }
+      end
+
+      it do
+        expect(ProconBypassMan::SendErrorCommand).to receive(:execute)
+        expect(action).to receive(:be_failed)
+        expect(action).to receive(:be_in_progress)
+        expect(action).not_to receive(:be_processed)
+        subject
+      end
+
+      it do
+        expect(ProconBypassMan::SendErrorCommand).to receive(:execute)
+        subject
+      end
+    end
+  end
+end
