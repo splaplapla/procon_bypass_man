25eb8910bb331449b298cb2d8e75e75ffb73a9bc
jiikko <n905i.1214@gmail.com>
2021-12-11 01:41:20 +0900

    ポーリングをwebsocketにする

--- lib/procon_bypass_man/websocket/pbm_job_client.rb
+++ lib/procon_bypass_man/websocket/pbm_job_client.rb
@@ -18,9 +18,18 @@ module ProconBypassMan
           )
           client.connected { puts 'successfully connected.' }
 
-          client.received do |message|
+          client.received do |data|
+            pbm_job_hash = data["message"]
+            pbm_job_object = ProconBypassMan::RemotePbmActionObject.new(action: pbm_job_hash["action"],
+                                                                        status: pbm_job_hash["status"],
+                                                                        uuid: pbm_job_hash["uuid"],
+                                                                        created_at: pbm_job_hash["created_at"],
+                                                                        job_args: pbm_job_hash["args"])
             ProconBypassMan.logger.info message
-            # TODO ProconBypassMan::FetchAndRunRemotePbmActionJob 相当のことをやる
+            pbm_job_object.validate!
+            ProconBypassMan::RunRemotePbmActionDispatchCommand.execute(action: pbm_job_object.action, uuid: pbm_job_object.uuid, job_args: pbm_job_object.job_args)
+          rescue ProconBypassMan::RemotePbmActionObject::ValidationError => e
+            ProconBypassMan::SendErrorCommand.execute(error: e)
           end
 
           client.connected {
