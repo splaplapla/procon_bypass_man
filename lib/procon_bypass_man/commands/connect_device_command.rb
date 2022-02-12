787bc036a483f3eec3786c06cfe52d85b37793dd
jiikko <n905i.1214@gmail.com>
2022-02-13 01:22:47 +0900

    add test

--- lib/procon_bypass_man/commands/connect_device_command.rb
+++ lib/procon_bypass_man/commands/connect_device_command.rb
@@ -5,17 +5,19 @@ class ProconBypassMan::ConnectDeviceCommand
       raise ProconBypassMan::NotFoundRequiredFilesError, "there is not /sys/kernel/config/usb_gadget/procon"
     end
 
-    gadget, procon = ProconBypassMan::DeviceConnector.connect
-  rescue ProconBypassMan::DeviceConnector::NotFoundProconError => e
-    ProconBypassMan.logger.error e
-    gadget&.close
-    procon&.close
-    raise ProconBypassMan::NotFoundProconError
-  rescue ProconBypassMan::SafeTimeout::Timeout
-    ProconBypassMan.logger.error "デバイスとの通信でタイムアウトが起きて接続ができませんでした。"
-    gadget&.close
-    procon&.close
-    raise ::ProconBypassMan::EternalConnectionError
+    begin
+      gadget, procon = ProconBypassMan::DeviceConnector.connect
+    rescue ProconBypassMan::DeviceConnector::NotFoundProconError => e
+      ProconBypassMan.logger.error e
+      gadget&.close
+      procon&.close
+      raise ProconBypassMan::NotFoundProconError
+    rescue ProconBypassMan::SafeTimeout::Timeout
+      ProconBypassMan.logger.error "デバイスとの通信でタイムアウトが起きて接続ができませんでした。"
+      gadget&.close
+      procon&.close
+      raise ProconBypassMan::EternalConnectionError
+    end
   end
 
   def self.has_required_files?
--- spec/lib/procon_bypass_man/commands/connect_device_command_spec.rb
+++ spec/lib/procon_bypass_man/commands/connect_device_command_spec.rb
@@ -2,12 +2,24 @@ require "spec_helper"
 
 describe ProconBypassMan::ConnectDeviceCommand do
   describe '.execute!' do
+    subject { described_class.execute! }
+
     before do
       allow(ProconBypassMan::DeviceConnector).to receive(:connect)
     end
 
     it do
-      expect { described_class.execute! }.not_to raise_error
+      expect { subject }.not_to raise_error
+    end
+
+    context 'when no has_required_files' do
+      before do
+        allow(described_class).to receive(:has_required_files?) { false }
+      end
+
+      it do
+        expect { subject }.to raise_error(ProconBypassMan::NotFoundRequiredFilesError)
+      end
     end
 
     context 'when timeout' do
@@ -16,17 +28,18 @@ describe ProconBypassMan::ConnectDeviceCommand do
       end
 
       it do
-        expect { described_class.execute! }.to raise_error(::ProconBypassMan::EternalConnectionError)
+        expect { subject }.to raise_error(::ProconBypassMan::EternalConnectionError)
       end
     end
 
     context 'when procon not found ' do
       before do
         allow(ProconBypassMan::DeviceConnector).to receive(:connect) { raise ProconBypassMan::DeviceConnector::NotFoundProconError }
+        allow(ProconBypassMan).to receive(:logger) { Logger.new("/dev/null") }
       end
 
       it do
-        expect { described_class.execute! }.to raise_error(ProconBypassMan::NotFoundProconError)
+        expect { subject }.to raise_error(ProconBypassMan::NotFoundProconError)
       end
     end
   end
