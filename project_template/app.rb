50c192e222563db5de7252a11c9c69e70b6f30a6
jiikko <n905i.1214@gmail.com>
2022-02-14 00:16:44 +0900

    コメントにverbose bypass logについて言及する

--- project_template/app.rb
+++ project_template/app.rb
@@ -12,11 +12,18 @@ ProconBypassMan.configure do |config|
   config.root = File.expand_path(__dir__)
   config.logger = Logger.new("#{ProconBypassMan.root}/app.log", 5, 1024 * 1024 * 10)
   config.logger.level = :debug
+
+  # バイパスするログを全部app.logに流すか
+  # config.verbose_bypass_log = true
+
   # webからProconBypassManを操作できるwebサービス
   # config.api_servers = ['https://pbm-cloud.herokuapp.com']
+
   config.enable_critical_error_logging = true
+
   # pbm-cloudで使う場合はnever_exitにtrueをセットしてください. trueがセットされている場合、不慮の事故が発生してもプロセスが終了しなくなります
   config.never_exit_accidentally = true
+
   # 操作が高頻度で固まるときは、 gadget_to_procon_interval の数値は大きくしてください
   config.bypass_mode = { mode: :normal, gadget_to_procon_interval: 5 }
 end
