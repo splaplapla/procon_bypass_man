22001c47848134cc964c337ff41ceaccc9d2c63d
jiikko <n905i.1214@gmail.com>
2022-02-26 11:20:17 +0900

    メソッドをまとめる

--- lib/procon_bypass_man/procon/macro.rb
+++ lib/procon_bypass_man/procon/macro.rb
@@ -1,9 +1,25 @@
 class ProconBypassMan::Procon::Macro
-  class OnetimeNestedStep
+  class BaseNestedStep
     def initialize(value)
       @hash = value
     end
 
+    private
+
+    def incr_step_index!
+      if step_index
+        @hash[:step_index] += 1
+      else
+        @hash[:step_index] = 0
+      end
+    end
+
+    def current_step
+      @hash[:steps][step_index]
+    end
+  end
+
+  class OnetimeNestedStep < BaseNestedStep
     def over?
       current_step.nil?
     end
@@ -16,32 +32,20 @@ class ProconBypassMan::Procon::Macro
 
     private
 
-    def current_step
-      @hash[:steps][step_index]
-    end
-
     def step_index
       @hash[:step_index] ||= 0
     end
-
-    def incr_step_index!
-      if step_index
-        @hash[:step_index] += 1
-      else
-        @hash[:step_index] = 0
-      end
-    end
   end
 
-  class NestedStep
+  class NestedStep < BaseNestedStep
     def initialize(value)
-      @hash = value
+      super
       unless @hash[:end_at]
         @hash[:end_at] = (Time.now + @hash[:continue_for]).round(4)
       end
     end
 
-    def over_end_at?
+    def over?
       (@hash[:end_at] < Time.now).tap do |result|
         if result
           ProconBypassMan.logger.debug { "[Macro] nested step is finished(#{@hash})" }
@@ -51,8 +55,6 @@ class ProconBypassMan::Procon::Macro
 
     def next_step
       incr_step_index!
-
-      debug_incr_called_count!
       if step = current_step
         return step
       else
@@ -63,29 +65,12 @@ class ProconBypassMan::Procon::Macro
 
     private
 
-    def current_step
-      @hash[:steps][step_index]
-    end
-
     def step_index
       @hash[:step_index]
     end
 
-    def incr_step_index!
-      if step_index
-        @hash[:step_index] += 1
-      else
-        @hash[:step_index] = 0
-      end
-    end
-
     def reset_step_index!
       @hash[:step_index] = 0
-    end
-
-    def debug_incr_called_count!
-      @hash[:debug_called_count] ||= 0
-      @hash[:debug_called_count] += 1
     end
   end
 
@@ -103,22 +88,18 @@ class ProconBypassMan::Procon::Macro
     end
 
     if step.is_a?(Hash)
-      if step[:continue_for]
-        nested_step = NestedStep.new(step)
-        if nested_step.over_end_at?
-          steps.shift # NestedStepを破棄する
-          return next_step
+      nested_step =
+        if step[:continue_for]
+          NestedStep.new(step)
         else
-          return nested_step.next_step
+          OnetimeNestedStep.new(step)
         end
+
+      if nested_step.over?
+        steps.shift # NestedStepを破棄する
+        return next_step
       else
-        nested_step = OnetimeNestedStep.new(step)
-        if nested_step.over?
-          steps.shift # NestedStepを破棄する
-          return next_step
-        else
-          return nested_step.next_step
-        end
+        return nested_step.next_step
       end
     end
   end
