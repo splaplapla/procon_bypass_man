fa49bf3c814b4b4624227da04cd6f7833ef59d71
jiikko <n905i.1214@gmail.com>
2022-03-06 16:47:34 +0900

    log

--- lib/procon_bypass_man/bypass/usb_hid_logger.rb
+++ lib/procon_bypass_man/bypass/usb_hid_logger.rb
@@ -28,6 +28,8 @@ class ProconBypassMan::Bypass
         end
       end
 
+      ProconBypassMan.logger.debug { bypass_value.binary.to_procon_reader.to_hash }
+
       if ProconBypassMan.config.enable_reporting_pressed_buttons
         ProconBypassMan.cache.fetch key: 'pressed_buttons_reporter', expires_in: 5 do
           ProconBypassMan::ReportPressedButtonsJob.perform_async(
