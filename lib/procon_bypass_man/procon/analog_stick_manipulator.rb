0eafdd5249211d16821b796447a9b2c63d40f04e
jiikko <n905i.1214@gmail.com>
2022-03-06 16:56:31 +0900

    fix position

--- lib/procon_bypass_man/procon/analog_stick_manipulator.rb
+++ lib/procon_bypass_man/procon/analog_stick_manipulator.rb
@@ -27,24 +27,24 @@ class ProconBypassMan::Procon::AnalogStickManipulator
 
       case @direction
       when :left
-        @manipulated_x = 0
-        @manipulated_y = 1808
+        @manipulated_abs_x = 400
+        @manipulated_abs_y = 1808
       when :right
-        @manipulated_x = 5000
-        @manipulated_y = 1808
+        @manipulated_abs_x = 3400
+        @manipulated_abs_y = 1808
       end
     else
       warn "error stick manipulator"
       analog_stick = ProconBypassMan::Procon::AnalogStick.new(binary: binary)
-      @manipulated_x = analog_stick.abs_x
-      @manipulated_y = analog_stick.abs_y
+      @manipulated_abs_x = analog_stick.abs_x
+      @manipulated_abs_y = analog_stick.abs_y
     end
   end
 
   def to_binary
     Position.new(
-      x: @manipulated_x,
-      y: @manipulated_y,
+      x: @manipulated_abs_x,
+      y: @manipulated_abs_y,
     ).to_binary
   end
 end
