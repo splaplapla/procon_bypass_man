df20bce6a2cf991e92dfc05ccf2083713738ef69
jiikko <n905i.1214@gmail.com>
2022-05-11 17:28:30 +0900

    bypass前の処理を行うクラスを追加する

--- lib/procon_bypass_man/device_connection.rb
+++ lib/procon_bypass_man/device_connection.rb
@@ -4,4 +4,5 @@ module ProconBypassMan::DeviceConnection
 end
 
 require_relative "device_connection/executor"
+require_relative "device_connection/pre_bypass"
 require_relative "device_connection/command"
---
+++ lib/procon_bypass_man/device_connection/pre_bypass.rb
@@ -0,0 +1,9 @@
+class ProconBypassMan::DeviceConnection::PreBypass
+  def initialize(gadget: , procon: )
+    @gadget = gadget
+    @procon = procon
+  end
+
+  def execute!
+  end
+end
