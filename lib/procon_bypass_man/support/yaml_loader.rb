448dea840873633c8e80b89a72a78bbd08829a7b
jiikko <n905i.1214@gmail.com>
2022-05-14 13:57:29 +0900

    Use strip!

--- lib/procon_bypass_man/support/yaml_loader.rb
+++ lib/procon_bypass_man/support/yaml_loader.rb
@@ -4,7 +4,9 @@ class ProconBypassMan::YamlLoader
   def self.load(path: )
     YAML.load_file(path).tap do |y|
       # 行末に空白があるとto_yamlしたときに改行コードがエスケープされてしまうのでstrip
-      y.transform_values! { |v| v.strip }
+      y.transform_values do |v|
+        v.strip! if v.is_a?(String)
+      end
     end
   end
 end
