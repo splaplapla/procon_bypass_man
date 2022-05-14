7c51c634ae2818a7b73d75f042853e4e4d108ed7
jiikko <n905i.1214@gmail.com>
2022-05-14 18:05:18 +0900

    add class

---
+++ lib/procon_bypass_man/device_connection/procon_setting_overrider.rb
@@ -0,0 +1,8 @@
+class ProconBypassMan::DeviceConnection::ProconSettingOverrider
+  def initialize(procon: )
+    @procon = procon
+  end
+
+  def execute!
+  end
+end
---
+++ spec/lib/procon_bypass_man/device_connection/procon_setting_overrider_spec.rb
@@ -0,0 +1,6 @@
+require "spec_helper"
+
+describe ProconBypassMan::DeviceConnection::ProconSettingOverrider do
+  describe '#execute!' do
+  end
+end
