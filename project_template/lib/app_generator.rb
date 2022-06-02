ee0e56ffb298cd2d384b3d642e20d253f320e29f
jiikko <n905i.1214@gmail.com>
2022-06-03 06:00:40 +0900

    classにする

---
+++ project_template/lib/app_generator.rb
@@ -0,0 +1,29 @@
+require "erb"
+
+class AppGenerator
+  attr_reader :path
+
+  def initialize(path: , enable_integration_with_pbm_cloud: )
+    @path = path
+    @enable_integration_with_pbm_cloud = enable_integration_with_pbm_cloud
+  end
+
+  def generate
+    erb = File.read(File.join(path, template_path))
+    enable_integration_with_pbm_cloud = @enable_integration_with_pbm_cloud
+    app_rb = ERB.new(erb, trim_mode: '-').result(binding)
+    File.write(File.join(path, output_path), app_rb)
+
+    puts "Created project_template/app.rb"
+  end
+
+  private
+
+  def template_path
+    "project_template/app.rb.erb"
+  end
+
+  def output_path
+    "project_template/app.rb"
+  end
+end
