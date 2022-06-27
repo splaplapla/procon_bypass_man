4a3dc0f2bfb654cf5c4b3e8fbb5a58297b0fa860
jiikko <n905i.1214@gmail.com>
2022-06-27 20:14:00 +0900

    log

--- lib/procon_bypass_man/procon_display/bypass_hook.rb
+++ lib/procon_bypass_man/procon_display/bypass_hook.rb
@@ -6,6 +6,9 @@ module ProconBypassMan::ProconDisplay::BypassHook
   set_callback :log_procon_to_gadget, :after, :write_procon_display_Status
 
   def write_procon_display_Status
-    ProconBypassMan::ProconDisplay::Status.instance.current = bypass_value.binary.to_procon_reader.to_hash.dup
+    ProconBypassMan.cache.fetch key: 'pressed_buttons_reporter', expires_in: 5 do
+      ProconBypassMan.logger.debug { "!!!!!!!!!!!!!!!!!!!!!!!!!!(#{bypass_value.binary.to_procon_reader.to_hash})" }
+      ProconBypassMan::ProconDisplay::Status.instance.current = bypass_value.binary.to_procon_reader.to_hash.dup
+    end
   end
 end
