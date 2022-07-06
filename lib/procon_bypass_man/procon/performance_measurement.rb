6a34a38247e8979f2422dc3e2cdae7ad7d5dbd27
jiikko <n905i.1214@gmail.com>
2022-07-06 19:49:33 +0900

    空配列の時に対応する

--- lib/procon_bypass_man/procon/performance_measurement.rb
+++ lib/procon_bypass_man/procon/performance_measurement.rb
@@ -30,7 +30,7 @@ module ProconBypassMan::Procon::PerformanceMeasurement
       time_taken_p50 = percentile(sorted_list: sorted_time_taken, percentile: 0.50)
       time_taken_p95 = percentile(sorted_list: sorted_time_taken, percentile: 0.95)
       time_taken_p99 = percentile(sorted_list: sorted_time_taken, percentile: 0.99)
-      max_time_taken = sorted_time_taken.last
+      max_time_taken = sorted_time_taken.last || 0
       total_read_error_count = @measurements.map(&:read_error_count).sum
       total_write_error_count = @measurements.map(&:write_error_count).sum
       PerformanceMetrics.new(time_taken_p50, time_taken_p95, time_taken_p99, max_time_taken, total_read_error_count, total_write_error_count)
@@ -42,6 +42,7 @@ module ProconBypassMan::Procon::PerformanceMeasurement
     # @param [Float] percentile
     # @return [Float]
     def percentile(sorted_list: , percentile: )
+      return 0.0 if sorted_list.empty?
       values_sorted = sorted_list
       k = (percentile*(values_sorted.length-1)+1).floor - 1
       f = (percentile*(values_sorted.length-1)+1).modulo(1)
--- spec/lib/procon_bypass_man/procon/performance_measurement_spec.rb
+++ spec/lib/procon_bypass_man/procon/performance_measurement_spec.rb
@@ -38,21 +38,35 @@ describe ProconBypassMan::Procon::PerformanceMeasurement do
 
   describe '.summarize' do
     let(:measurement_class) { Struct.new(:time_taken, :read_error_count, :write_error_count) }
-    let(:measurements) do
-      [ measurement_class.new(1, 1, 2),
-        measurement_class.new(4, 1, 2),
-        measurement_class.new(2, 1, 2),
-        measurement_class.new(3, 1, 2),
-      ]
-    end
 
     subject { described_class.summarize(measurements: measurements) }
 
-    it { expect(subject.time_taken_p50).to eq(2.5) }
-    it { expect(subject.time_taken_p95).to eq(3.849) }
-    it { expect(subject.time_taken_p99).to eq(3.969) }
-    it { expect(subject.max_time_taken).to eq(4) }
-    it { expect(subject.read_error_count).to eq(4) }
-    it { expect(subject.write_error_count).to eq(8) }
+    context '値があるとき' do
+      let(:measurements) do
+        [ measurement_class.new(1, 1, 2),
+          measurement_class.new(4, 1, 2),
+          measurement_class.new(2, 1, 2),
+          measurement_class.new(3, 1, 2),
+        ]
+      end
+
+      it { expect(subject.time_taken_p50).to eq(2.5) }
+      it { expect(subject.time_taken_p95).to eq(3.849) }
+      it { expect(subject.time_taken_p99).to eq(3.969) }
+      it { expect(subject.max_time_taken).to eq(4) }
+      it { expect(subject.read_error_count).to eq(4) }
+      it { expect(subject.write_error_count).to eq(8) }
+    end
+
+    context '空配列のとき' do
+      let(:measurements) { [] }
+
+      it { expect(subject.time_taken_p50).to eq(0) }
+      it { expect(subject.time_taken_p95).to eq(0) }
+      it { expect(subject.time_taken_p99).to eq(0) }
+      it { expect(subject.max_time_taken).to eq(0) }
+      it { expect(subject.read_error_count).to eq(0) }
+      it { expect(subject.write_error_count).to eq(0) }
+    end
   end
 end
