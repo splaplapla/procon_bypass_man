75b0aaa5a6af406243d3e79419735e19162f4265
jiikko <n905i.1214@gmail.com>
2022-09-07 08:11:10 +0900

    add frozen_string_literal: true

--- lib/procon_bypass_man/configuration.rb
+++ lib/procon_bypass_man/configuration.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ProconBypassMan::Configuration
   module ClassMethods
     def root
--- lib/procon_bypass_man/support/callbacks.rb
+++ lib/procon_bypass_man/support/callbacks.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ProconBypassMan
   module CallbacksRegisterable
     attr_accessor :callbacks
