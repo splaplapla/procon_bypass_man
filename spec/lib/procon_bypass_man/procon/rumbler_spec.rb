e2f238a1e0c467240f0e1f92081222c37aa61c4a
jiikko <n905i.1214@gmail.com>
2022-09-11 18:51:49 +0900

    add spec

---
+++ spec/lib/procon_bypass_man/procon/rumbler_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+describe ProconBypassMan::Procon::Rumbler do
+  describe '.monitor' do
+    context 'ブロックの中で何もしないとき' do
+      it do
+        ProconBypassMan::Procon::Rumbler.monitor {}
+        expect(ProconBypassMan::Procon::Rumbler.must_rumble?).to eq(false)
+      end
+    end
+    
+    context 'ブロックの中でrumble!を呼び出すとき' do
+      it do
+        ProconBypassMan::Procon::Rumbler.monitor {
+         ProconBypassMan::Procon::Rumbler.rumble!
+        }
+        expect(ProconBypassMan::Procon::Rumbler.must_rumble?).to eq(true)
+      end
+    end
+  end
+end
