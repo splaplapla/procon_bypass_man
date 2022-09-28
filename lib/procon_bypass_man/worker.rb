a98a95c4387936996e52d0881fb9c3fd70dc3464
jiikko <n905i.1214@gmail.com>
2022-09-28 23:18:47 +0900

    add spec for worker

--- lib/procon_bypass_man/worker.rb
+++ lib/procon_bypass_man/worker.rb
@@ -6,27 +6,30 @@ module ProconBypassMan
       new.run
     end
 
-    # @param [Boolean]
+    # @return [Worker]
     def run
       return self if @thread
       @thread = Thread.new do
         while(item = ProconBypassMan::Background::JobQueue.pop)
-          begin
-            # プロセスを越えるので、文字列でenqueueしてくれる前提. evalしてクラスにする
-            job_class = eval(item[:job_class])
-            ProconBypassMan::Background::JobPerformer.new(klass: job_class, args: item[:args]).perform
-          rescue => e
-            ProconBypassMan.logger.error(e)
-            if job_class.respond_to?(:re_enqueue_if_failed) && job_class.re_enqueue_if_failed
-              job_class.perform_async(item[:args])
-            end
-
-            sleep(0.2) # busy loopしないように
-          end
+          job_class = eval(item[:job_class])
+          work(job_class: job_class, args: item[:args])
+          sleep(0.2) # busy loopしないように
         end
       end
 
       return self
+    end
+
+    def work(job_class: , args: )
+      begin
+        # プロセスを越えるので、文字列でenqueueしてくれる前提. evalしてクラスにする
+        job_class.perform(*args)
+      rescue => e
+        ProconBypassMan.logger.error(e)
+        if job_class.respond_to?(:re_enqueue_if_failed) && job_class.re_enqueue_if_failed
+          job_class.perform_async(args)
+        end
+      end
     end
 
     # 重要な非同期ジョブは存在しないのでqueueが捌けるのを待たずにkill
