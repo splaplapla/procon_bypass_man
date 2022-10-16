8ee1e5913da109e1f6f0a1bccbedbef63f7e19f1
jiikko <n905i.1214@gmail.com>
2022-10-16 10:38:18 +0900

    サードパーティコントローラーが刺さっていればそれを使う

--- lib/procon_bypass_man/support/device_procon_finder.rb
+++ lib/procon_bypass_man/support/device_procon_finder.rb
@@ -1,19 +1,34 @@
 class ProconBypassMan::DeviceProconFinder
   HID_NAME = "Nintendo Co., Ltd. Pro Controller"
 
+  THIRD_PARTY_DEVICE_PATH = "/dev/input/js0"
+
   def self.find
     new.find
   end
 
   # @return [String, NilClass]
   def find
-    find_device_path
+    if(procon_path = find_procon_path)
+      return procon_path
+    end
+
+    if(third_party_device_path = find_third_party_device_path)
+      return third_party_device_path
+    end
   end
 
   private
 
   # @return [String , NilClass]
-  def find_device_path
+  def find_third_party_device_path
+    if File.exists?(THIRD_PARTY_DEVICE_PATH)
+      return THIRD_PARTY_DEVICE_PATH
+    end
+  end
+
+  # @return [String , NilClass]
+  def find_procon_path
     if(line = device_from_shell) && (hidraw_name = line.match(/(hidraw\d+)\s+/)[1])
       "/dev/#{hidraw_name}"
     end
