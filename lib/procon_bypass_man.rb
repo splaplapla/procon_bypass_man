188632fcb2e3a07f0413129ce02fdf937f13af07
jiikko <n905i.1214@gmail.com>
2022-11-07 20:35:29 +0900

    BlueGreenProcess.shared_variablesに左スティックの値を受け入れるようにする

--- lib/procon_bypass_man.rb
+++ lib/procon_bypass_man.rb
@@ -195,7 +195,7 @@ module ProconBypassMan
         ProconBypassMan::RemoteMacroReceiver.start!
         BlueGreenProcess.config.logger = ProconBypassMan.logger
       }
-      config.shared_variables = [:buttons, :current_layer_key]
+      config.shared_variables = [:buttons, :current_layer_key, :recent_left_stick_positions]
     end
   end
 
