0265445acb7c74ab1ec22a3d522d358b15ceff0e
jiikko <n905i.1214@gmail.com>
2022-11-20 19:48:31 +0900

    fixup! ベンチマークスクリプトを追加する

--- benchmarks/bypass_b.rb
+++ benchmarks/bypass_b.rb
@@ -6,9 +6,37 @@ setting_content =
   <<~EOH
     version: 1.0
     setting: |-
-      prefix_keys_for_changing_layer [:zr, :r, :zl, :l]
-      layer :up do
-        flip :zr, if_pressed: :zr
+      enable(:rumble_on_layer_change)
+
+      install_macro_plugin ProconBypassMan::Plugin::Splatoon3::Macro::FastReturn
+      install_macro_plugin ProconBypassMan::Plugin::Splatoon3::Macro::JumpToUpKey
+      install_macro_plugin ProconBypassMan::Plugin::Splatoon3::Macro::JumpToRightKey
+      install_macro_plugin ProconBypassMan::Plugin::Splatoon3::Macro::JumpToLeftKey
+      install_macro_plugin ProconBypassMan::Plugin::Splatoon3::Macro::SokuwariForSplashBomb
+      install_mode_plugin ProconBypassMan::Plugin::Splatoon3::Mode::Guruguru
+
+      install_macro_plugin ProconBypassMan::Plugin::Splatoon3::Macro::DaseiCancel
+
+      prefix_keys_for_changing_layer [:zr, :zl, :l]
+      set_neutral_position 1906, 1886
+
+      layer :up, mode: :manual do
+        flip :zr, if_pressed: :zr, force_neutral: :zl
+        flip :zl, if_pressed: [:y, :b, :zl]
+        flip :a, if_pressed: [:a]
+        flip :down, if_pressed: :down
+        macro ProconBypassMan::Plugin::Splatoon3::Macro::FastReturn, if_pressed: [:y, :b, :down]
+        macro ProconBypassMan::Plugin::Splatoon3::Macro::JumpToUpKey, if_pressed: [:y, :b, :up]
+        macro ProconBypassMan::Plugin::Splatoon3::Macro::JumpToRightKey, if_pressed: [:y, :b, :right]
+        macro ProconBypassMan::Plugin::Splatoon3::Macro::JumpToLeftKey, if_pressed: [:y, :b, :left]
+
+        remap :l, to: :zr
+        open_macro :shake, steps: [:shake_left_stick_and_toggle_b_for_0_1sec], if_pressed: [:b, :r], force_neutral: [:b]
+        left_analog_stick_cap cap: 1100, if_pressed: [:zl, :a], force_neutral: :a
+        open_macro :forward_ikarole, steps: [:forward_ikarole1], if_pressed: [:thumbl], force_neutral: []
+        disable_macro :all, if_pressed: :a
+        disable_macro :all, if_pressed: :zr
+        macro ProconBypassMan::Plugin::Splatoon3::Macro::DaseiCancel, if_tilted_left_stick:  { threshold: 700 }, if_pressed: [:zl]
       end
   EOH
 setting = Tempfile.new
