3dcc54ed2cb50f1516279289d9853d9e227b1907
jiikko <n905i.1214@gmail.com>
2023-04-02 22:58:25 +0900

    log

--- lib/procon_bypass_man/procon.rb
+++ lib/procon_bypass_man/procon.rb
@@ -197,7 +197,7 @@ class ProconBypassMan::Procon
     if(data = ProconBypassMan::ExternalInput.read)
       begin
         external_data = ProconBypassMan::ExternalInput::ExternalData.parse!(data)
-        ProconBypassMan.logger.debug { "[ExternalInput] 読み取った値: { hex: #{external_data.hex}, buttons: #{external_data.buttons} }" }
+        ProconBypassMan.logger.debug { "[ExternalInput] 読み取った値: { hex: #{external_data.hex}, buttons: #{external_data.buttons} }, #{data.encoding}, #{Encoding.default_external}, " }
 
         if(external_data_raw_binary = external_data.to_binary)
           self.user_operation.merge(external_data_raw_binary)
