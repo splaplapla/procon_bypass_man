cf040411f73e41a0e7952fb5f07ae3209ed9804c
jiikko <n905i.1214@gmail.com>
2023-04-02 10:58:19 +0900

    カンマ区切りの入力も受け付けれるようにする

--- spec/lib/procon_bypass_man/external_input/external_data_spec.rb
+++ spec/lib/procon_bypass_man/external_input/external_data_spec.rb
@@ -4,27 +4,55 @@ describe ProconBypassMan::ExternalInput::ExternalData do
   describe '.parse!' do
     subject { described_class.parse!(data) }
 
-    context '空のとき' do
-      let(:data) { {}.to_json }
+    context 'not jsonのとき' do
+      context '無効な文字を含むカンマ区切り' do
+        let(:data) { 'a,b,c' }
 
-      it 'returns data instance' do
-        expect(subject).to have_attributes(hex: nil, buttons: [])
+        it 'returns data instance' do
+          expect(subject).to have_attributes(hex: nil, buttons: [:a, :b, :c])
+        end
+      end
+
+      context 'カンマ区切り' do
+        let(:data) { 'a,b' }
+
+        it 'returns data instance' do
+          expect(subject).to have_attributes(hex: nil, buttons: [:a, :b])
+        end
+      end
+
+      context '空文字' do
+        let(:data) { '' }
+
+        it 'returns data instance' do
+          expect(subject).to have_attributes(hex: nil, buttons: [])
+        end
       end
     end
 
-    context 'hexがあるとき' do
-      let(:data) { { hex: 'a' }.to_json }
+    context 'jsonのとき' do
+      context '空のとき' do
+        let(:data) { {}.to_json }
 
-      it 'returns data instance' do
-        expect(subject).to have_attributes(hex: 'a', buttons: [])
+        it 'returns data instance' do
+          expect(subject).to have_attributes(hex: nil, buttons: [])
+        end
       end
-    end
 
-    context 'buttonsがあるとき' do
-      let(:data) { { hex: 'a', buttons: [:a, :b] }.to_json }
+      context 'hexがあるとき' do
+        let(:data) { { hex: 'a' }.to_json }
 
-      it 'returns data instance' do
-        expect(subject).to have_attributes(hex: 'a', buttons: [:a, :b])
+        it 'returns data instance' do
+          expect(subject).to have_attributes(hex: 'a', buttons: [])
+        end
+      end
+
+      context 'buttonsがあるとき' do
+        let(:data) { { hex: 'a', buttons: [:a, :b] }.to_json }
+
+        it 'returns data instance' do
+          expect(subject).to have_attributes(hex: 'a', buttons: [:a, :b])
+        end
       end
     end
   end
