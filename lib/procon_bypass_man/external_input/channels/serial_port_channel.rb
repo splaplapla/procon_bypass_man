e7425f837c25d0a65e3689360cb760fea91ee94e
jiikko <n905i.1214@gmail.com>
2023-04-02 11:16:01 +0900

    read_nonblockだとバッファを途中までしか読めないことがある対策

--- lib/procon_bypass_man/external_input/channels/serial_port_channel.rb
+++ lib/procon_bypass_man/external_input/channels/serial_port_channel.rb
@@ -18,11 +18,29 @@ module ProconBypassMan
         end
 
         # @return [String, NilClass]
+        # NOTE: この実装では、::IO::EAGAINWaitReadableを実質的な終端として扱っているので、高速に書き込みがされると取りこぼす可能性がある.
+        # NOTE: read_nonblockでバッファから読み出すとき、バッファが空になるまでは::IO::EAGAINWaitReadableが起きない前提で実装している.
+        # NOTE: 取りこぼししないよう精度を上げるには、終端文字が来るまで1文字ずつ読む必要があるがパフォーマンスが犠牲になってしまう. この対策をするには、bypass処理の開始で非同期に1文字ずつ読み込むことをすると多少マシになるはず
         def read
-          # NOTE: 改行コードが来るまでバッファリングが必要？
-          @serial_port.read_nonblock(1024)
-        rescue ::IO::EAGAINWaitReadable
-          nil
+          buffer = ''
+          loop do
+            begin
+              buffer += @serial_port.read_nonblock(1024) || ''
+            rescue ::IO::EAGAINWaitReadable
+              break
+            end
+          end
+
+          return nil if buffer.empty?
+
+          # NOTE: 高速に書き込まれた場合、複数のチャンクを含む可能性があるので、最初だけを切り取る
+          chunks = buffer.split("\n")
+          if(chunks.size > 1)
+            ProconBypassMan::SendErrorCommand.execute(
+              error: "[ExternalInput] シリアルポートから読み込んだchunkが複数あります. 高い書き込み頻度に耐えられていないので実装を見直してください。 (chunks.size: #{chunks.size})"
+            )
+          end
+          chunks.first
         end
       end
     end
