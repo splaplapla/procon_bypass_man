4a2a3e28f61adfe10c00ad6b27e11a330ab1d7d1
jiikko <n905i.1214@gmail.com>
2023-04-02 11:16:01 +0900

    read_nonblockだとバッファを途中までしか読めないことがある対策

--- spec/lib/procon_bypass_man/external_input/channels/serial_port_spec.rb
+++ spec/lib/procon_bypass_man/external_input/channels/serial_port_spec.rb
@@ -1,13 +1,15 @@
 require "spec_helper"
+require "serialport"
 
 describe ProconBypassMan::ExternalInput::Channels::SerialPortChannel do
   describe '.read' do
     subject { described_class.new(device_path: '/dev/null').read }
 
-    context 'readできないのとき' do
+    context 'readできたとき' do
       before do
         serial_port = double(:serial_port)
         expect(serial_port).to receive(:read_nonblock) { 'foo' }
+        expect(serial_port).to receive(:read_nonblock) { raise ::IO::EAGAINWaitReadable }
         allow(::SerialPort).to receive(:new) { serial_port }
       end
 
@@ -16,7 +18,47 @@ describe ProconBypassMan::ExternalInput::Channels::SerialPortChannel do
       end
     end
 
-    context 'readできたとき' do
+    context '改行を含むreadできたとき' do
+      before do
+        serial_port = double(:serial_port)
+        expect(serial_port).to receive(:read_nonblock) { "foo\n" }
+        expect(serial_port).to receive(:read_nonblock) { raise ::IO::EAGAINWaitReadable }
+        allow(::SerialPort).to receive(:new) { serial_port }
+      end
+
+      it do
+        expect(subject).to eq('foo')
+      end
+    end
+
+    context '複数の要素を含むreadできたとき' do
+      before do
+        serial_port = double(:serial_port)
+        expect(serial_port).to receive(:read_nonblock) { "foo\nbar" }
+        expect(serial_port).to receive(:read_nonblock) { raise ::IO::EAGAINWaitReadable }
+        allow(::SerialPort).to receive(:new) { serial_port }
+      end
+
+      it do
+        expect(subject).to eq('foo')
+      end
+    end
+
+    context '複数回readできたとき' do
+      before do
+        serial_port = double(:serial_port)
+        expect(serial_port).to receive(:read_nonblock) { "foo" }
+        expect(serial_port).to receive(:read_nonblock) { "bar" }
+        expect(serial_port).to receive(:read_nonblock) { raise ::IO::EAGAINWaitReadable }
+        allow(::SerialPort).to receive(:new) { serial_port }
+      end
+
+      it do
+        expect(subject).to eq('foobar')
+      end
+    end
+
+    context 'readできないのとき' do
       before do
         serial_port = double(:serial_port)
         allow(serial_port).to receive(:read_nonblock) { raise ::IO::EAGAINWaitReadable }
