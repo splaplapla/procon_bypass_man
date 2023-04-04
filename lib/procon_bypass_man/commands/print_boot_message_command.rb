97f6df22fbc663650ab8299e3f514156014b232f
jiikko <n905i.1214@gmail.com>
2023-04-05 00:21:26 +0900

    boot messageをapp.logにも出す

--- lib/procon_bypass_man/commands/print_boot_message_command.rb
+++ lib/procon_bypass_man/commands/print_boot_message_command.rb
@@ -57,6 +57,7 @@ class ProconBypassMan::PrintBootMessageCommand
   def self.execute
     message = BootMessage.new
     ProconBypassMan::ReportBootJob.perform_async(message.to_hash)
+    ProconBypassMan.logger.info message.to_s
     puts message.to_s
   end
 end
