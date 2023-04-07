7d28750f262d80b2c8a7f6bb8b01cd8ff523990d
jiikko <n905i.1214@gmail.com>
2023-04-07 21:10:57 +0900

    tcp serverの仮実装をする

--- lib/procon_bypass_man/external_input/channels/tcpip_channel.rb
+++ lib/procon_bypass_man/external_input/channels/tcpip_channel.rb
@@ -2,9 +2,48 @@ module ProconBypassMan
   module ExternalInput
     module Channels
       class TCPIPChannel < Base
+        class TCPServer < EventMachine::Connection
+          @@queue = Queue.new
+
+          def post_init
+            puts "A client has connected"
+          end
+
+          def unbind
+            puts "A client has disconnected"
+          end
+
+          def receive_data(data)
+            puts "#{data}を受け取ったよ(#{data.codepoints})"
+
+            case data
+            when /^{/
+              @@queue.push(data)
+              send_data "OK\r\n"
+            when "\r\n"
+              if @@queue.size.zero?
+                send_data "EMPTY\r\n"
+                return
+              end
+
+              data = @@queue.pop
+              send_data "#{data}\r\n"
+            else
+              send_data "Unknown command\r\n"
+            end
+          end
+        end
+
         def initialize(port: )
           super()
-          # TODO: ここでTCPサーバを起動する. masterプロセスをサーバとして、bypassプロセスはクライアントとして振る舞う
+
+          Thread.start do
+            ProconBypassMan::Websocket::Forever.run do
+              EventMachine.run do
+                EventMachine.start_server '0.0.0.0', port, EchoServer
+              end
+            end
+          end
         end
 
         def read
